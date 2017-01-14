c629274ec1baa0ea86ca226e7ef64208485a4034
Robert Schütte <schuetterobert@gmail.com>
2017-01-14 16:32:33 +0100

    Added the controller and node class

--- src/basic/mod.rs
+++ src/basic/mod.rs
@@ -9,7 +9,7 @@
 //! to a Z-Wave network, to send  messages and to receive them.
 
 
-use driver::Driver;
+use driver::{Driver, GenericType};
 
 use std::rc::Rc;
 use std::cell::RefCell;
@@ -17,12 +17,10 @@ use cc::CmdClass;
 
 use error::Error;
 
-
-
 /************************** Controller Area *********************/
 
 /// The private representation of a controller
-struct _Controller<D> where D: Driver {
+struct _Controller<D> where D: Driver+Clone {
     driver: D,
     nodes: Vec<Node<D>>,
 }
@@ -31,10 +29,10 @@ struct _Controller<D> where D: Driver {
 type ControllerRef<D> = Rc<RefCell<_Controller<D>>>;
 
 // The public representation of a controller, with some syntactic sugar.
-pub struct Controller<D>(ControllerRef<D>) where D: Driver;
+pub struct Controller<D>(ControllerRef<D>) where D: Driver+Clone;
 
 // The actual controller implementation
-impl<D> Controller<D> where D: Driver {
+impl<D> Controller<D> where D: Driver+Clone {
     /// Creates a new controller
     pub fn new(driver: D) -> Result<Controller<D>, Error> {
         let controller = Controller(Rc::new(RefCell::new(_Controller {
@@ -43,51 +41,29 @@ impl<D> Controller<D> where D: Driver {
         })));
 
         // discover nodes automaically
-        let nd = controller.discover_nodes();
-        if nd.is_some() {
-            return Err(nd.unwrap());
-        }
+        controller.discover_nodes()?;
 
         Ok(controller)
     }
 
     /// Refresh the complete node list
-    pub fn discover_nodes(&self) -> Option<Error> {
+    pub fn discover_nodes(&self) -> Result<(), Error> {
         // clear the existing nodes
-        /*self.0.borrow_mut().nodes.clear();
+        self.0.borrow_mut().nodes.clear();
 
         // write out the descovery node command and receive the answer
-        let res = self.0.borrow().driver.write_and_read(Message::new(Type::Request, Function::DiscoveryNodes, vec!()));
+        let res = self.0.borrow_mut().driver.get_node_ids()?;
 
-        // when a error occoured return it
-        let res = unwrap_or_return!(res.clone().ok(), res.err());
-
-        // grab the data
-        let data = res.data;
-
-        // check if the data is long enough and if the right bit is set
-        if data.len() != 34 || data[2] != 0x1D {
-            return Some(Error::new(ErrorKind::UnknownZWave, "The ZWave message has a wrong format"));
+        // create a node object for each id
+        for i in res {
+            // create the node for the given id
+            let n = Node::new(self.clone(), i as u8);
+            // add it !!! need 2 steps to prevent collition with borrow
+            self.0.borrow_mut().nodes.push(n);
         }
 
-        // loop over each bitmask byte
-        for i in 3..31 {
-            // loop over each bit of the byte
-            for j in 0..7 {
-                // check if the bit is set
-                if Controller::get_bit_at(data[i], j) {
-                    // calc the number out of the bitmask
-                    let n = ((i-3) * 8) + (j as usize+1);
-                    // create the node for the given id
-                    let n = Node::new(self.clone(), n as u8);
-                    // add it !!! need 2 steps to prevent collition with borrow
-                    self.0.borrow_mut().nodes.push(n);
-                }
-            }
-        }*/
-
         // when everything went well, return no error
-        None
+        Ok(())
     }
 
     /// Returns a vector of all available nodes
@@ -111,19 +87,15 @@ impl<D> Controller<D> where D: Driver {
         None
     }
 
-    /*** private functions below this line ***/
-
-    /// Checks if the bit at the requested position is set
-    fn get_bit_at(input: u8, n: u8) -> bool {
-        if n < 8 {
-            input & (1 << n) != 0
-        } else {
-            false
-        }
+    /// returns the driver for this controller
+    pub fn get_driver(&self) -> D {
+        self.0.borrow().driver.clone()
     }
 }
 
-impl<D> Clone for Controller<D> where D: Driver {
+/// The clone function need to be written manually
+/// beacause of a 'bug' in rust.
+impl<D> Clone for Controller<D> where D: Driver+Clone {
     fn clone(&self) -> Controller<D> {
         Controller(self.0.clone())
     }
@@ -132,7 +104,7 @@ impl<D> Clone for Controller<D> where D: Driver {
 /************************** Node Area *********************/
 
 // The private representation of a node
-struct _Node<D> where D: Driver {
+struct _Node<D> where D: Driver+Clone {
     id: u8,
     controller: Controller<D>,
     generic_type: GenericType,
@@ -143,10 +115,10 @@ struct _Node<D> where D: Driver {
 type NodeRef<D> = Rc<RefCell<_Node<D>>>;
 
 // The public representation of a node, with some syntactic sugar.
-pub struct Node<D>(NodeRef<D>) where D: Driver;
+pub struct Node<D>(NodeRef<D>) where D: Driver+Clone;
 
 // The actual node implementation
-impl<D> Node<D> where D: Driver {
+impl<D> Node<D> where D: Driver+Clone {
     /// Creates a new node with no edges.
     pub fn new(contr: Controller<D>, id: u8) -> Node<D> {
         let node = Node(Rc::new(RefCell::new(_Node {
@@ -156,30 +128,23 @@ impl<D> Node<D> where D: Driver {
             class_basic: None
         })));
 
-        node.discover_type();
+        // we need to handle to spress the warning,
+        // wiich can't be deactivated until today
+        if node.discover_type().is_err() {
+            node.0.borrow_mut().generic_type = GenericType::Unknown;
+        }
         node.discover_classes();
 
         node
     }
 
     /// Sets the available type for this node
-    pub fn discover_type(&self) {
-        /*let mut this = &mut self.0.borrow_mut();
+    pub fn discover_type(&self) -> Result<(), Error> {
+        let mut this = &mut self.0.borrow_mut();
 
-        // create a new message
-        let msg = Message::new(Type::Request, Function::GetNodeProtocolInfo, vec!(this.id));
+        this.generic_type = this.controller.get_driver().get_node_generic_class(&this.id)?;
 
-        // send the message to the ZWave driver
-        let msg = unwrap_or_return!(this.controller.get_driver().write_and_read(msg).ok(), ());
-
-        //check if the answer has the right length && is no error
-        if msg.data.len() != 6 {
-            this.generic_type = GenericType::Unknown;
-            return;
-        }
-
-        // extract the delivered type
-        this.generic_type = GenericType::from_u8(msg.data[4]).unwrap_or(GenericType::Unknown);*/
+        Ok(())
     }
 
     /// Sets the available function classes for this node
@@ -208,43 +173,8 @@ impl<D> Node<D> where D: Driver {
     }
 }
 
-impl<D> Clone for Node<D> where D: Driver {
+impl<D> Clone for Node<D> where D: Driver+Clone {
     fn clone(&self) -> Node<D> {
         Node(self.0.clone())
     }
-}
-
-/// List of different Node types
-enum_from_primitive! {
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum GenericType {
-    Unknown = 0x00,
-    RemoteController = 0x01,
-    StaticController = 0x02,
-    AvControlPoint = 0x03,
-    RoutingSlave = 0x04,
-    Display = 0x06,
-    GarageDoor = 0x07,
-    WindowCovering = 0x09,
-    Thermostat = 0x08,
-    RepeaterSlave = 0x0F,
-    BinarySwitch = 0x10,
-    MultiLevelSwitch = 0x11,
-    RemoteSwitch = 0x12,
-    ToggleSwitch = 0x13,
-    ZIpGateway = 0x14,
-    ZIpNode = 0x15,
-    Ventilation = 0x16,
-    GenericSecurityPanel = 0x17,
-    RemoteSwitch2 = 0x18,
-    BinarySensor = 0x20,
-    MultilevelSensor = 0x21,
-    WaterControl = 0x22,
-    PulseMeter = 0x30,
-    Meter = 0x31,
-    EntryControl = 0x40,
-    SemiInteroperable = 0x50,
-    AlarmSensor = 0xa1,
-    NonInteroperable = 0xFF,
-}
 }
